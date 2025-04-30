bb8e5b9642e53a7247e38bd3f4fca690020a164f
lau184 <agostinacod@gmail.com>
2025-04-30 19:55:53 -0300

    Update lau.py
    valide la moneda y q el monto sea +

--- lau.py
+++ lau.py
@@ -6,3 +6,14 @@ tas_cambios= {
     "ARS": {"USD": 0.0011, "EUR": 0.00105, "BRL": 0.0058},
     "BRL": {"USD": 0.20, "EUR": 0.18, "ARS": 172.00}
 }
+money = ["USD", "EUR", "ARS", "BRL"]
+
+def vali_money(moneda):
+    return moneda.upper() in monedas
+  
+def vali_monto(monto):
+    try:
+        valor = float(monto)
+        return valor > 0
+    except ValueError:
+        return False
