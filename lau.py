61ab081c751f08665fde486e32125837251e855c
lau184 <agostinacod@gmail.com>
2025-04-30 19:26:09 -0300

    Update lau.py
    Puse las tasaw de cambios

--- lau.py
+++ lau.py
@@ -1,5 +1,8 @@
-print ("hola") 
-print("hello") 
+import datetime
 
-
-print("codoco")
+tas_cambios= {
+  "USD": {"EUR": 0.92, "ARS": 880.00, "BRL": 5.10},
+    "EUR": {"USD": 1.09, "ARS": 950.00, "BRL": 5.50},
+    "ARS": {"USD": 0.0011, "EUR": 0.00105, "BRL": 0.0058},
+    "BRL": {"USD": 0.20, "EUR": 0.18, "ARS": 172.00}
+}
